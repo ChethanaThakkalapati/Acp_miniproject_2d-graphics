0b45a0a8b1781cc0f0d2a939a0cbcd31d26e4316
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2026-06-08 20:05:15 +0530

    modify,delete function and error corrections

--- miniproject.c
+++ miniproject.c
@@ -9,7 +9,7 @@ char canvas[ROWS][COLS];
 typedef struct
 {
     int type; // 1-Line 2-Rectangle 3-Circle 4-Triangle
-    int x1, y1, x2, y2, r;
+    int x1, y1, x2, y2, x3, y3, r;
 } Object;
 //object Array
 Object objects[MAX_OBJECTS];
@@ -49,6 +49,11 @@ void drawLine(int x1,int y1,int x2,int y2)
     int dx = abs(x2 - x1);
     int dy = abs(y2 - y1);
     int steps = (dx > dy) ? dx : dy;
+    if (steps == 0)
+    {
+        plotPoint(x1, y1);
+        return;
+    }
     float Xinc = (float)(x2 - x1) / steps;
     float Yinc = (float)(y2 - y1) / steps;
     float x = x1;
@@ -74,8 +79,8 @@ void drawCircle(int x0,int y0,int r)
     for(int angle=0;angle<360;angle++)
     {
         float rad=angle*3.14159/180;
-        int x=xc+r*cos(rad);
-        int y=yc+r*sin(rad);
+        int x=x0+r*cos(rad);
+        int y=y0+r*sin(rad);
 
         plotPoint(x,y);
     }
@@ -106,7 +111,7 @@ void redrawCanvas()
             drawCircle(obj.x1, obj.y1, obj.r);
             break;
         case 4:
-            drawTriangle(obj.x1, obj.y1, obj.x2, obj.y2, obj.r, obj.y2); 
+            drawTriangle(obj.x1, obj.y1, obj.x2, obj.y2, obj.x3, obj.y3); 
             break;
         }
     }
@@ -114,6 +119,11 @@ void redrawCanvas()
 //Add function
 void addline()
 {
+    if(objectCount>=MAX_OBJECTS)
+    {
+        printf("Maximum object limit reached!\n");
+        return;
+    }
     Object obj;
     obj.type = 1;
     printf("Enter x1 y1 x2 y2: ");
@@ -122,6 +132,11 @@ void addline()
 }
 void addRectangle()
 {
+    if(objectCount>=MAX_OBJECTS)
+    {
+        printf("Maximum object limit reached!\n");
+        return;
+    }
     Object obj;
     obj.type = 2;
     printf("Enter top-left and bottom-right coordinates:\n ");
@@ -130,6 +145,11 @@ void addRectangle()
 }
 void addCircle()
 {
+    if(objectCount>=MAX_OBJECTS)
+    {
+        printf("Maximum object limit reached!\n");
+        return;
+    }
     Object obj;
     obj.type = 3;
     printf("Enter center x y and radius: ");
@@ -138,10 +158,65 @@ void addCircle()
 }
 void addTriangle()
 {
+    if(objectCount>=MAX_OBJECTS)
+    {
+        printf("Maximum object limit reached!\n");
+        return;
+    }
     Object obj;
     obj.type = 4;
     printf("Enter x1 y1 x2 y2 x3 y3:\n ");
-    scanf("%d %d %d %d %d %d", &obj.x1, &obj.y1, &obj.x2, &obj.y2, &obj.r, &obj.y2); 
+    scanf("%d %d %d %d %d %d", &obj.x1, &obj.y1, &obj.x2, &obj.y2, &obj.x3, &obj.y3);
     objects[objectCount++] = obj;
 }
+//delete function
+void deleteObject()
+{
+    int index;
+    printf("Enter object index to delete: ");
+    scanf("%d", &index);
+    if (index < 0 || index >= objectCount)
+    {
+        printf("Invalid object index!\n");
+        return;
+    }
+    for (int i = index; i < objectCount - 1; i++)
+    {
+        objects[i] = objects[i + 1];
+    }
+        objectCount--;
+}
+// modify function
+void modifyObject()
+{
+    int index;
+    printf("Enter object index to modify: ");
+    scanf("%d", &index);
+    if (index < 0 || index >= objectCount)
+    {
+        printf("Invalid object index!\n");
+        return;
+    }
+    Object *obj = &objects[index];
+    switch (obj->type)
+    {
+    case 1:
+        printf("Enter new x1 y1 x2 y2: ");
+        scanf("%d %d %d %d", &obj->x1, &obj->y1, &obj->x2, &obj->y2);
+        break;
+    case 2:
+        printf("Enter new rectangle coordinates:\n ");
+        scanf("%d %d %d %d", &obj->x1, &obj->y1, &obj->x2, &obj->y2);
+        break;
+    case 3:
+        printf("Enter new center and radius: ");
+        scanf("%d %d %d", &obj->x1, &obj->y1, &obj->r);
+        break;
+    case 4:
+        printf("Enter new x1 y1 x2 y2 x3 y3:\n ");
+        scanf("%d %d %d %d %d %d", &obj->x1, &obj->y1, &obj->x2, &obj->y2, &obj->x3, &obj->y3);
+        break;
+    }
+}
+
 
